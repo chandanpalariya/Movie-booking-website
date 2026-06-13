9411f99f9baf43ca993ec08e11d9bfac216bd9b6
ChandanPalariya <Chandan0406@nexzem.com>
2026-06-13 09:18:49 +0530

    some fixes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const app=express();
 const port=process.env.PORT||5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin:"*",
+    credentials: true
+
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
