5f841f4bb04c00f6cc5cc72dd26147d1a9c20f3b
chandanpalariya <cpalariya67@gmail.com>
2026-06-13 11:34:18 +0530

    fixes of urls

--- frontent/src/pages/MovieDetailPage.jsx
+++ frontent/src/pages/MovieDetailPage.jsx
@@ -15,7 +15,7 @@ import {
 import { toast } from "react-toastify";
 import { movieDetailHStyles } from "../assests/dummyStyles";
 
-const API_BASE = "http://localhost:5000";
+const API_BASE = "https://movie-booking-website-1-zob1.onrender.com";
 const ROWS = [
   { id: "A", type: "standard", count: 8 },
   { id: "B", type: "standard", count: 8 },
